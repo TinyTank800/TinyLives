2fb9d5329237173053078677a97e17fea2ca380a
TinyTank800 <tinytank800@gmail.com>
2024-08-04 15:11:42 -0700

    Fix for colors not parsing in lives command

--- src/main/java/com/tinytank800/tinylives/Commands/Handlers/LivesHandler.java
+++ src/main/java/com/tinytank800/tinylives/Commands/Handlers/LivesHandler.java
@@ -14,21 +14,19 @@ public class LivesHandler {
     public static void Lives(@NotNull Player sender, @NotNull Command command, @NotNull String label, String[] args){
         int lives = 0;
 
-        Player player = null;
-
         if(args.length >= 1){
             if(sender.hasPermission("tinylives.lives.others")){
-                player = Tinylives.getInstance().getServer().getPlayer(args[0]);
+                Player player = Tinylives.getInstance().getServer().getPlayer(args[0]);
                 if(player == null){
                     return;
                 }
 
                 lives = Configs.Get("playerdata.yml").getInt("players." + player.getUniqueId() + ".lives");
-                sender.sendMessage(Configs.Get("config.yml").getString("prefix") + "&a " + player.getName() + " currently has &f" + lives + " live/s&a.");
+                sender.sendMessage(Parser.format(sender,Configs.Get("config.yml").getString("prefix") + "&a " + player.getName() + " currently has &f" + lives + " live/s&a."));
             }
         } else {
             lives = Configs.Get("playerdata.yml").getInt("players." + sender.getUniqueId() + ".lives");
-            sender.sendMessage(Configs.Get("config.yml").getString("prefix") + "&a you currently have &f" + lives + " live/s&a.");
+            sender.sendMessage(Parser.format(sender,Configs.Get("config.yml").getString("prefix") + "&a you currently have &f" + lives + " lives&a."));
         }
     }
 }
